82d6099fbd33b305c53551f34435b5aa582102d0
Aphilosopher30 <ashafer1@fordham.edu>
2022-06-04 22:49:29 -0600

    set up plan for routs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
+# add transaction
+  create 'user/transactions/new', to: 'transaction#create'
 
+# spend points according to ruesl
+  get 'user/transactions/spend', to: 'user#spend'
+
+# list all payers
+  get '/user/payers', to: 'user#payers'
 
 
 
