653289c062d43ca0595400a56c45925eade4add5
Aphilosopher30 <ashafer1@fordham.edu>
2022-06-07 17:26:24 -0600

    add capacity to handle illegitimate time stamps

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,19 +1,17 @@
 class TransactionController < ApplicationController
 
   def create
-# binding.pry
-    data = {payer: params[:payer], points: params[:points].to_i, time_stamp: Time.parse(params[:time_stamp])}
-    if data[:time_stamp].class != Time
+
+    data = {payer: params[:payer], points: params[:points].to_i}
+    begin
+      data[:time_stamp] = Time.parse(params[:time_stamp])
+    rescue ArgumentError
       data[:time_stamp] = Time.now
     end
 
     new_transaction = Transaction.new(data)
 
-
-# binding.pry
-
     render json: TransactionSerializer.new(new_transaction)
-
   end
 
 
--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe 'transactions request ', type: :request do
 
   describe "create a transaction" do
-
     describe "gives a proper response " do
       it 'creates a new transaction ' do
 
@@ -13,21 +12,39 @@ RSpec.describe 'transactions request ', type: :request do
         expect(response.status).to eq(200)
         expect(response.content_type).to eq("application/json")
 
-        picture_data = JSON.parse(response.body, symbolize_names: true)
+        transaction_data = JSON.parse(response.body, symbolize_names: true)
 
-        expect(picture_data[:data][:id]).to eq(nil)
-        expect(picture_data[:data][:type]).to eq("transaction")
-        expect(picture_data[:data][:attributes][:payer]).to eq("abbs Corp")
-        expect(picture_data[:data][:attributes][:points]).to eq(500)
-        expect(picture_data[:data][:attributes][:time_stamp]).to eq("2020-10-31T11:00:00.000Z")
+        expect(transaction_data[:data][:id]).to eq(nil)
+        expect(transaction_data[:data][:type]).to eq("transaction")
+        expect(transaction_data[:data][:attributes][:payer]).to eq("abbs Corp")
+        expect(transaction_data[:data][:attributes][:points]).to eq(500)
+        expect(transaction_data[:data][:attributes][:time_stamp]).to eq("2020-10-31T11:00:00.000Z")
       end
-
-
     end
 
     describe "sad paths" do
+      describe "gives a proper response " do
+        it 'creates a new transaction ' do
+
+          time = Time.new
+          allow(Time).to receive(:now).and_return(time)
+
+          post '/user/transactions/new', params: {payer: "abbs Corp", points: 500, time_stamp: "asdfasdf"}
 
 
+          expect(response).to be_successful
+          expect(response.status).to eq(200)
+          expect(response.content_type).to eq("application/json")
+
+          transaction_data = JSON.parse(response.body, symbolize_names: true)
+
+          expect(transaction_data[:data][:id]).to eq(nil)
+          expect(transaction_data[:data][:type]).to eq("transaction")
+          expect(transaction_data[:data][:attributes][:payer]).to eq("abbs Corp")
+          expect(transaction_data[:data][:attributes][:points]).to eq(500)
+          expect(Time.parse(transaction_data[:data][:attributes][:time_stamp])).to eq(time.round(3))
+        end
+      end
     end
 
 
