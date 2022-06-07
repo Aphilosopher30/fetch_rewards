ea85089abeca72d35baec062196d1a00f7f35af0
Aphilosopher30 <ashafer1@fordham.edu>
2022-06-07 17:47:25 -0600

    add to tests to ensure that we are increasing number of transactions tracked

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe 'transactions request ', type: :request do
     describe "gives a proper response " do
       it 'creates a new transaction ' do
 
+        prior_transactions = Transaction.all.count
+
         post '/user/transactions/new', params: {payer: "abbs Corp", points: 500, time_stamp: "2020-10-31T11:00:00Z"}
 
         expect(response).to be_successful
@@ -14,13 +16,17 @@ RSpec.describe 'transactions request ', type: :request do
 
         transaction_data = JSON.parse(response.body, symbolize_names: true)
 
+
         expect(transaction_data[:data][:id]).to eq(nil)
         expect(transaction_data[:data][:type]).to eq("transaction")
         expect(transaction_data[:data][:attributes][:payer]).to eq("abbs Corp")
         expect(transaction_data[:data][:attributes][:points]).to eq(500)
         expect(transaction_data[:data][:attributes][:time_stamp]).to eq("2020-10-31T11:00:00.000Z")
+
+        expect(Transaction.all.count).to eq(prior_transactions+1)
       end
       it 'can handle not being given a time_stamp' do
+        prior_transactions = Transaction.all.count
 
         time = Time.now.round(3)
         allow(Time).to receive(:now).and_return(time)
@@ -39,8 +45,11 @@ RSpec.describe 'transactions request ', type: :request do
         expect(transaction_data[:data][:attributes][:payer]).to eq("abbs Corp")
         expect(transaction_data[:data][:attributes][:points]).to eq(500)
         expect(Time.parse(transaction_data[:data][:attributes][:time_stamp])).to eq(time)
+
+        expect(Transaction.all.count).to eq(prior_transactions+1)
       end
       it 'can handle illegitimate time stamp' do
+        prior_transactions = Transaction.all.count
 
         time = Time.now.round(3)
         allow(Time).to receive(:now).and_return(time)
@@ -59,6 +68,8 @@ RSpec.describe 'transactions request ', type: :request do
         expect(transaction_data[:data][:attributes][:payer]).to eq("abbs Corp")
         expect(transaction_data[:data][:attributes][:points]).to eq(500)
         expect(Time.parse(transaction_data[:data][:attributes][:time_stamp])).to eq(time)
+
+        expect(Transaction.all.count).to eq(prior_transactions+1)
       end
 
 
