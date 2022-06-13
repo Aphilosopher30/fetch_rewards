1ab30d2a78f5f7ba109d645be157649e4d76287f
aphilosopher30 <>
2022-06-12 18:32:45 -0600

    add tests

--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'transactions request ', type: :request do
 
         prior_transactions = Transaction.all.count
 
-        post '/user/transactions/new', params: {payer: "abbs Corp", points: 500, time_stamp: "2020-10-31T11:00:00Z"}
+        post '/user/transactions/new', params: {payer: "abbs Corp", points: '500', time_stamp: "2020-10-31T11:00:00Z"}
 
         expect(response).to be_successful
         expect(response.status).to eq(200)
@@ -32,7 +32,7 @@ RSpec.describe 'transactions request ', type: :request do
         time = Time.now.round(3)
         allow(Time).to receive(:now).and_return(time)
 
-        post '/user/transactions/new', params: {payer: "abbs Corp", points: 500}
+        post '/user/transactions/new', params: {payer: "abbs Corp", points: '500'}
 
 
         expect(response).to be_successful
@@ -76,9 +76,9 @@ RSpec.describe 'transactions request ', type: :request do
     end
 
     describe "sad paths" do
-
-
     end
+
+
   end
 
 
@@ -148,7 +148,39 @@ RSpec.describe 'transactions request ', type: :request do
     end
 
 
+
   end
 
+  describe "sad paths for spending points" do
+    it 'lets you know if you dont have sufficent funds ' do
+
+      Transaction.delete_all
+
+      data0 = { payer: "DANNON", points: 1000, time_stamp: Time.parse("2020-11-02T14:00:00Z") }#5
+      data1 = { payer: "UNILEVER", points: 200, time_stamp: Time.parse("2020-10-31T11:00:00Z") }#2
+      data2 = { payer: "DANNON", points: -200, time_stamp: Time.parse("2020-10-31T15:00:00Z") }#3
+      data3 = { payer: "MILLER COORS", points: 10000, time_stamp: Time.parse("2020-11-01T14:00:00Z") }#4
+      data4 = { payer: "DANNON", points: 300, time_stamp: Time.parse("2020-10-31T10:00:00Z") }#1
+
+      transaction0 = Transaction.new(data0)
+      transaction1 = Transaction.new(data1)
+      transaction2 = Transaction.new(data2)
+      transaction3 = Transaction.new(data3)
+      transaction4 = Transaction.new(data4)
+
+      get '/user/transactions/spend', params: { points: 1000000}
+
+      expect(response.status).to eq(400)
+      expect(response.content_type).to eq("application/json")
+
+      transaction_data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(transaction_data[:error]).to eq("insufficent funds")
+    end
+
+  end
+
+
+  # expect(item[:data]).to have_key(:id)
 
 end
